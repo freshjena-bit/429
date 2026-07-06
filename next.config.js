ca0b6629cee5db63cbfe00c6b028d0813724e48b
freshjena-bit <freshjena@gmail.com>
2026-07-06 15:52:33 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Aktifkan jika Anda menggunakan middleware API route
+  // experimental: {
+  //   middleware: true,
+  // },
+};
+
+module.exports = nextConfig;
